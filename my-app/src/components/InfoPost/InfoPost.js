d414b964564e19d30c6c386560108d723a132be3
cristian9805 <58344164+cristian9805@users.noreply.github.com>
2023-12-02 07:51:23 -0600

    boton editar al 100%

--- my-app/src/components/InfoPost/InfoPost.js
+++ my-app/src/components/InfoPost/InfoPost.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstr
 import { Modal, Button } from 'react-bootstrap'; // Asegúrate de importar el Modal y Button de react-bootstrap
 //import { useNavigate } from 'react-router-dom';
 
-const VerPost=() => {
+const VerPost = () => {
   const [postData, setpostData] = useState([]);
   const [userData, setuserData] = useState([]);
   const [showModal, setShowModal] = useState(false);
@@ -24,10 +24,13 @@ const VerPost=() => {
   const [editedTitle, setEditedTitle] = useState('');
   const [editedBody, setEditedBody] = useState('');
 
+  //ESTA CONSTANTE ES PARA FORZAR A REACT A REFRESCAR LA PANTALLA YA QUE AL CAMBIAR UN VALOR SE REFRESCA.
+  //const [contador, setContador] = useState(0);
+  //const [contador, setContador] = useState(false);
   //const navigate = useNavigate(); //SE UNSA EN EL METODO DE 'confirmDelete'
 
 
-  const idPost = 'Uv7Y0cQz3LX2clfjCdQk'//ENVIAR EL ID DEL POST SELECIONADO DESDE EL HOME
+  const idPost = 'Yu5D3TxBhOQZuv35ns6h'//ENVIAR EL ID DEL POST SELECIONADO DESDE EL HOME
   localStorage.setItem('postId', idPost);//guardar en local esta linea la tengo que borrar luego.kassandra ya me va asubir el iddel post
 
   useEffect(() => {
@@ -43,9 +46,9 @@ const VerPost=() => {
           setInitialTitle(post.title); // Guardar el título inicial al cargar
           setInitialBody(post.body); // Guardar el cuerpo inicial al cargar
 
-          
+
           //BUSCAR DATOS DEL USUARIO POR MEDIO DEL USERID DEL POST CREADO
-          try { 
+          try {
             const user = await getUserDataPostById(post.userId);
             //console.log(user);
             if (user) {
@@ -73,8 +76,10 @@ const VerPost=() => {
 
   /********************************* EDITAR **********************************************/
   const handleEdit = () => {
+    setEditedTitle(postData[0].title);
+    setEditedBody(postData[0].body);
     setIsEditing(true);
-  };  
+  };
 
   const handleSave = async () => {
     try {
@@ -82,6 +87,14 @@ const VerPost=() => {
       console.log(editedTitle)
       console.log(editedBody)
       await updatePostById(postId, { title: editedTitle, body: editedBody });
+
+      // Actualizar el estado postData con los nuevos datos del post editado
+      setpostData((prevPosts) =>
+        prevPosts.map((post) =>
+          post.id === postId ? { ...post, title: editedTitle, body: editedBody } : post
+        )
+      );
+
       setIsEditing(false);
     } catch (error) {
       console.error('Error al guardar los cambios:', error);
@@ -93,7 +106,7 @@ const VerPost=() => {
     setEditedBody(initialBody); // Revertir los cambios al cuerpo original
     setIsEditing(false); // Salir del modo de edición
   };
-  
+
   /********************************* EDITAR **********************************************/
 
   /********************************* MODAL **********************************************/
@@ -116,11 +129,11 @@ const VerPost=() => {
     setComment(event.target.value);
     console.log('entro')
   };
-  
+
   /***********************************COMENTARIOS********************************/
   return (
-  <div className="ver-post-container">
-    <Modal show={showModal} onHide={handleCloseModal}>
+    <div className="ver-post-container">
+      <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>Confirmar eliminación</Modal.Title>
         </Modal.Header>
@@ -134,69 +147,69 @@ const VerPost=() => {
           </Button>
         </Modal.Footer>
       </Modal>
-    <div>
-      {postData.map((post) => (
-        <div className="post" key={post.id}>
-          {isEditing ? (
-            <>
-              <input
-                type="text"
-                value={editedTitle}
-                onChange={(e) => setEditedTitle(e.target.value)}
-                className="title-input" // Clase para mantener los estilos del título
-                placeholder="Título"
-              />
-              <textarea
-                value={editedBody}
-                onChange={(e) => setEditedBody(e.target.value)}
-                rows={4}
-                cols={50}
-                className="body-textarea" // Clase para mantener los estilos del cuerpo del post
-                placeholder="Cuerpo del post..."
-              />
-              <div className="button-container">
-              <button onClick={handleSave} className="btn btn-primary mt-2" >Guardar cambios</button>
-              <button onClick={handleCancel} className="btn btn-danger mt-2">Cancelar</button>
-              </div>
-            </>
-          ) : (
-          <>
-          <h3 className="post-title">{post.title}</h3>
-          <p className="post-body">{post.body}</p>
-          {userData.map((user) =>(
-            <p className='p-Autor'>{user.name}</p>
-          ))}
-          
-          <div className="comment-section">
-            {/* Caja de texto para el comentario */}
-            <textarea
-              value={comment}
-              onChange={handleCommentChange}
-              placeholder="Agrega tu comentario..."
-              rows={4}
-              cols={50}
-              className="comment-textarea" 
-            />
-            <button variant="success"
-            className="btn btn-success mt-2"
-            onClick={() => {
-              //console.log('Comentario enviado:', comment);
-              uploadComments(comment,postId,userId,userName)
-              setComment('');//LIMPIAR CAMPO
+      <div>
+        {postData.map((post) => (
+          <div className="post" key={post.id}>
+            {isEditing ? (
+              <>
+                <input
+                  type="text"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                  className="title-input" // Clase para mantener los estilos del título
+                  placeholder="Título"
+                />
+                <textarea
+                  value={editedBody}
+                  onChange={(e) => setEditedBody(e.target.value)}
+                  rows={4}
+                  cols={50}
+                  className="body-textarea" // Clase para mantener los estilos del cuerpo del post
+                  placeholder="Cuerpo del post..."
+                />
+                <div className="button-container">
+                  <button onClick={handleSave} className="btn btn-primary mt-2" >Guardar cambios</button>
+                  <button onClick={handleCancel} className="btn btn-danger mt-2">Cancelar</button>
+                </div>
+              </>
+            ) : (
+              <>
+                <h3 className="post-title">{post.title}</h3>
+                <p className="post-body">{post.body}</p>
+                {userData.map((user) => (
+                  <p className='p-Autor'>{user.name}</p>
+                ))}
 
-            }}>Enviar comentario</button>
+                <div className="comment-section">
+                  {/* Caja de texto para el comentario */}
+                  <textarea
+                    value={comment}
+                    onChange={handleCommentChange}
+                    placeholder="Agrega tu comentario..."
+                    rows={4}
+                    cols={50}
+                    className="comment-textarea"
+                  />
+                  <button variant="success"
+                    className="btn btn-success mt-2"
+                    onClick={() => {
+                      //console.log('Comentario enviado:', comment);
+                      uploadComments(comment, postId, userId, userName)
+                      setComment('');//LIMPIAR CAMPO
+
+                    }}>Enviar comentario</button>
+                </div>
+
+                <div className="button-container">
+                  <button className="btn btn-primary mt-2" onClick={() => handleEdit(post.id)}>Editar</button>
+                  <span style={{ margin: '0 5px' }}></span> {/* Solo Agrega un Espacio entre los botones de boostrap */}
+                  <button className="btn btn-danger mt-2" onClick={() => handleDelete(post.id)} disabled>Eliminar</button>
+                </div>
+              </>
+            )}
           </div>
-          
-          <div className="button-container">
-            <button className="btn btn-primary mt-2"  onClick={() => handleEdit(post.id)}>Editar</button>
-            <span style={{ margin: '0 5px' }}></span> {/* Solo Agrega un Espacio entre los botones de boostrap */}
-            <button className="btn btn-danger mt-2" onClick={() => handleDelete(post.id)} disabled>Eliminar</button>
-          </div>
-          </>
-          )}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 }
